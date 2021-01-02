73ece266354bcd9281f613e8191e02cc46092c4f
João Mangueira <jamjs7@gmail.com>
2021-01-02 16:50:33 -0300

    ususario sendo registrados no banco

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,28 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    });
+
+    User.hook('beforeCreate', function (user) {
+       user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     });
 
     return User;
