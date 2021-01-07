7aec73448283abaf5a6449e634ad59b0b2377444
João Mangueira <jamjs7@gmail.com>
2021-01-07 18:56:56 -0300

    criando relacionamento das tabelas

--- src/models/answers.js
+++ src/models/answers.js
@@ -17,5 +17,15 @@ module.exports = (sequelize, DataTypes) => {
         }
     });
 
+    Answer.associate = function (models) {
+        const { User } = models;
+
+        Answer.belongsTo(User, { as: 'questionUser', foreignKey: 'question_user_id' });
+        User.hasMany(Answer, { as: 'questionUser', foreignKey: 'question_user_id' });
+
+        Answer.belongsTo(User, { as: 'answerUser', foreignKey: 'answer_user_id' });
+        User.hasMany(Answer, { as: 'answerUser', foreignKey: 'answer_user_id' });
+    }
+
     return Answer;
 }
