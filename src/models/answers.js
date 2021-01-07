649f1f7acdc91c0ee7646a972d0d5f6f4ac78e72
João Mangueira <jamjs7@gmail.com>
2021-01-06 22:38:00 -0300

    model das perguntas criado

---
+++ src/models/answers.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports = (sequelize, DataTypes) => {
+    const Answer = sequelize.define('Answer', {
+        question: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        answer: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: '',
+        },
+
+        is_annon: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        }
+    });
+
+    return Answer;
+}
